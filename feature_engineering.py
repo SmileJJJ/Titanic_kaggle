55aed529557ffa8855c7c81dc81128498dd580db
GSGS_MECHREV <281206814@qq.com>
2019-02-17 22:55:19 +0800

    2019.2.17

--- feature_engineering.py
+++ feature_engineering.py
@@ -36,19 +36,15 @@ __mtime__ = 'None'
 """
 
 import pandas as pd
+from sklearn import linear_model
 
+data_train = pd.read_csv('DATASET/train.csv')
 # 数据预处理一 : 补充缺失值
 # 缺失：Age  Cabin  Embarked
-data_train = pd.read_csv('DATASET/train.csv')
-
-data_train['Age'].fillna(int(data_train['Age'].mean()),inplace=True)
-
+data_train['Age'].fillna(int(data_train['Age'].mean()), inplace=True)
 data_train.loc[(data_train['Cabin'].notnull()), 'Cabin'] = 'YES'
 data_train.loc[(data_train['Cabin'].isnull()), 'Cabin'] = 'NO'
-
 data_train['Embarked'].fillna('S',inplace=True)
-
-# print(data_train.info())
 
 # 数据预处理二 : 数据one-hot处理(哑变量处理类别数据)
 dummies_Cabin = pd.get_dummies(data_train['Cabin'], prefix='Cabin')
@@ -60,9 +56,37 @@ df = pd.concat([data_train, dummies_Cabin, dummies_Sex, dummies_Embarked, dummie
 df.drop(['Cabin', 'Sex', 'Embarked', 'Pclass'], axis=1, inplace=True)
 
 # 数据预处理三 : 数据数据标准差处理
+a = df.Age
+b = df.Fare
+df['Age_scaled'] = (a-a.mean())/(a.std(ddof=0))
+df['Fare_scaled'] = (b-b.mean())/(b.std(ddof=0))
+df = df.drop('Age', axis=1)
+df = df.drop('Fare', axis=1)
+
+# 用正则取出我们要的属性值
+train_df = df.filter(regex='Survived|Age_.*|SibSp|Parch|Fare_.*|Cabin_.*|Embarked_.*|Sex_.*|Pclass_.*')
+train_np = train_df.as_matrix()
+
+y = train_np[:, 0]
+x = train_np[:, 1:]
+
+# penalty:制定正则化策略
+# C:一个浮点数，它指定了惩罚系数的倒数。如果它的值越小，则正则化越大
+# tol：一个浮点数，指定判断迭代收敛与否的一个阈值
+# verbose：一个正数。用于开启/关闭迭代中间输出的日志。
+# clf = linear_model.LogisticRegression(C=1.0, penalty='l1', tol=1e-6, verbose=True)
+# clf.fit(x, y)
+
+data_test = pd.read_csv('DATASET/test.csv')
+
+# 测试数据预处理
+data_test['Age'].fillna(int(data_test['Age'].mean()), inplace=True)
+data_test.loc[(data_test['Cabin'].notnull()), 'Cabin'] = 'YES'
+data_test.loc[(data_test['Cabin'].isnull()), 'Cabin'] = 'NO'
+
+print(data_test.info())
 
 
-print(df.info())
 
 
 
